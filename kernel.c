175506cb56711d4427777a16cfa887d63e922938
David Benshachar <dbenshachar@gmail.com>
2025-10-15 21:21:34 -0700

    stash

--- kernel.c
+++ kernel.c
@@ -1,4 +1,5 @@
 #include <stdint.h>
+#include "commands.h"
 
 #define UART0_BASE 0x09000000UL
 #define UARTDR (*(volatile unsigned int *)(UART0_BASE + 0x00))
@@ -10,22 +11,42 @@ static void putc(char c){ while (UARTFR & TXFF) {} UARTDR = (unsigned)c; }
 static void puts(const char*s){ while(*s) putc(*s++); }
 static int  getc(void){ while (UARTFR & RXFE) {} return UARTDR & 0xFF; }
 
+char cmdbuf[256];
+uint8_t cmdlen = 0;
+
+void echo() {
+
+}
+
+void execute() {
+
+}
+
+void new_line() {
+    cmdbuf[0] = '\0';
+    cmdlen = 0;
+}
+
+void backspace() {
+    cmdbuf[cmdlen] = '\0';
+    cmdlen = (cmdlen > 0) ? cmdlen - 1 : 0;
+}
+
 /*Execute by running:
 chmod +x run.bash && ./run.bash
 */
 void kmain(void){
-    puts("Kernel is running!\n> ");
+    puts("Kernel\n> ");
+    
     for(;;){
         int c = getc();
-        if (c== '\n') {
+        if (c == '\n' || c == '\r') {
+            new_line();
             puts("\n> ");
             continue;
         }
-        if (c =='\r'){
-            puts("\n> ");
-            continue;
-        } 
-        if (c ==0x7F || c =='\b'){
+        if (c ==0x7F || c =='\b') {
+            backspace();
             puts("\b \b");
             continue;
         }
