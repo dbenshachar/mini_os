7877e39ca6544c0a8c21b04aa23b5aab2c92bd07
Mineinjava <65673396+Mineinjava@users.noreply.github.com>
2025-10-25 00:46:09 -0700

    major changes-- organize, gitignore, swap to riscv32i, move to make

---
+++ src/commands.h
@@ -0,0 +1,81 @@
+#ifndef COMMANDS_H
+#define COMMANDS_H
+
+#include <stdint.h>
+#include "files.h"
+
+#define MAX_PARAMS 32
+#define MAX_PARAM_LENGTH 16
+
+int echo(char *string) {
+    puts("\n");
+    puts(string);
+    return 1;
+}
+
+int quit() {
+    return 0;
+}
+
+int fmake(char* type, char* name) {
+    if (str_eq(type, "folder")) {
+        return fs_mkdir(name, target_folder);
+    }
+    if (str_eq(type, "file")) {
+        return fs_mkfile(name, target_folder);
+    }
+    puts("\nInvalid make type");
+    return 1;
+}
+
+uint8_t deserialize_params(char *cmd, char **params, uint8_t max_params) {
+    uint8_t count = 0;
+    char *p = cmd;
+
+    while (*p != '\0') {
+        while (*p == ' ') p++;
+        if (*p == '\0') break;
+
+        if (count >= max_params) break;
+        params[count++] = p;
+
+        while (*p != '\0' && *p != ' ') p++;
+        if (*p == '\0') break;
+
+        *p = '\0';
+        p++;
+    }
+    return count;
+}
+
+int execute(char* cmd) {
+    char *params[MAX_PARAMS];
+    int param_count = deserialize_params(cmd, params, MAX_PARAMS);
+    if (param_count == 0) {return 0;}
+
+    const char *exec_command = params[0];
+    if (str_eq(exec_command, "quit")) {return quit();}
+    if (str_eq(exec_command, "echo")) {return echo(params[1]);}
+    if (str_eq(exec_command, "finit")) {return fs_init();}
+    if (str_eq(exec_command, "freturn")) {fs_return(); puts("\n"); puts(target_folder->name);return 1;}
+    if (str_eq(exec_command, "fdir")) {puts("\n"); puts(target_folder->name);return 1;}
+    if (str_eq(exec_command, "fwipe")) {fs_wipe(target_folder); puts("\n"); puts("wiped "); puts(target_folder->name); return 1;}
+    if (str_eq(exec_command, "flist")) {puts("\n"); fs_ls(target_folder); return 1;}
+    if (str_eq(exec_command, "fmake")) {fmake(params[1], params[2]); return 1;}
+    if (str_eq(exec_command, "fchange")) {fs_cd(target_folder, params[1]); puts("\n"); puts(target_folder->name); return 1;}
+    if (str_eq(exec_command, "fread")) {
+        puts("\n");
+        puts(fs_read(get_file(params[1])));
+        return 1;
+    }
+    if (str_eq(exec_command, "fwrite")) {
+        file* f = get_file(params[1]);
+        if (!f) {return 0;}
+        fs_write(f, params[2]);
+        return 1;
+    }
+    puts("\nInvalid command!");
+    return 1;
+}
+
+#endif
---
+++ src/kernel.c
@@ -0,0 +1,50 @@
+#include "commands.h"
+#include <stdint.h>
+
+int done = 0;
+char cmdbuf[256];
+uint8_t cmdlen = 0;
+
+void new_line() {
+  cmdbuf[0] = '\0';
+  cmdlen = 0;
+}
+
+void backspace() {
+  cmdbuf[cmdlen] = '\0';
+  cmdlen = (cmdlen > 0) ? cmdlen - 1 : 0;
+}
+
+/*Execute by running:
+chmod +x run.bash && ./run.bash
+*/
+void kmain(void) {
+  fs_init();
+  fs_return();
+  puts("\n> ");
+
+  while (1) {
+    int c = getc();
+    if (c == '\n' || c == '\r') {
+      cmdbuf[cmdlen] = '\0';
+      cmdlen++;
+
+      done = !execute(cmdbuf);
+      if (done) {
+        puts("\nTried to quit!");
+        break;
+      }
+      new_line();
+      puts("\n> ");
+      continue;
+    }
+    if (c == 0x7F || c == '\b') {
+      backspace();
+      puts("\b \b");
+      continue;
+    }
+    cmdbuf[cmdlen] = c;
+    cmdlen++;
+    putc((char)c);
+  }
+}
